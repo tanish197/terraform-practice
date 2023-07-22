de25e42998c6cd00acbe235e68a5a8bd927f2dbd
Tanish <tanishj1907@gmail.com>
2023-07-22 22:55:56 +0530

    new-tf-update

--- terrraform-course/terraform-variables/variable1.tf
+++ terrraform-course/terraform-variables/variable1.tf
@@ -11,3 +11,32 @@ variable "content" {
 variable "devops_op_trainer" {}
 
 
+variable "content_map" {
+type = map
+default = {
+"content1" = "THis is first file"
+"content2" = "this is second one"
+}
+}
+
+
+variable "file_list" {
+type = list
+default = ["/home/ubuntu/terrraform-course/terraform-variables/file_1.txt", "/home/ubuntu/terrraform-course/terraform-variables/file_2.txt"]
+}
+
+variable "aws_ec2_object" {
+ type = object({
+        name = string
+        instances = number
+        keys = list(string)
+        ami = string
+})
+
+default = {
+   name = "test_ec2_instance"
+   instances = 4
+   keys = ["key1.pem", "key2.pem"]
+   ami = "ubuntu.afed34"
+}
+}
--- terrraform-course/terraform-variables/variables.tf
+++ terrraform-course/terraform-variables/variables.tf
@@ -1,11 +1,11 @@
 resource "local_file" "devops"{
- filename = "/home/ubuntu/terrraform-course/terraform-variables/variables.txt"
- content = "This is my first file"
+ filename = var.file_list[0]
+ content = var.content_map["content1"]
 }
 
 resource "local_file" "devops-var"{
-  filename = var.filename
-  content = var.content
+  filename = var.file_list[1]
+  content = var.content_map["content2"]
 }
 
 
@@ -13,7 +13,9 @@ output "devops_op_trainer" {
 value = var.devops_op_trainer
 }
 
-
+output "aws_ec2_instances" {
+value = var.aws_ec2_object
+}
 
 
 
